43cc0f5d670546559c789ecc208e01557b06fcdf
Luis Carlin <luiscarlin@gmail.com>
2021-08-21 08:06:53 -0400

    add global styling

--- apps/web/components/Header.tsx
+++ apps/web/components/Header.tsx
@@ -1,10 +1,24 @@
 import Link from 'next/link';
+import styled from 'styled-components';
 import Nav from './Nav';
 
+const Logo = styled.h1`
+  font-size: 4rem;
+  background: red;
+  transform: skew(-7deg);
+
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+  }
+`;
 const Header = () => {
   return (
     <header>
-      <Link href="/">Sick Fits</Link>
+      <Logo>
+        <Link href="/">Sick Fits</Link>
+      </Logo>
       <p>Search</p>
       <Nav />
     </header>
