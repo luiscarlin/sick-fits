55b1b2a68e71b2ad5983b472872a2e177d7eb017
Luis Carlin <luiscarlin@gmail.com>
2021-08-24 07:10:36 -0400

    add custom nprogress

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,11 +1,13 @@
 import type { AppProps } from 'next/app';
 import Page from '../components/Page';
+import ProgressBar from '../components/ProgressBar';
 import GlobalStyles from '../styles/GlobalStyles';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <GlobalStyles />
+      <ProgressBar />
       <Page>
         <Component {...pageProps} />
       </Page>
